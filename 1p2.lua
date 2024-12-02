1f68f3b8d0469a9833fe0b3502758f96dd129a49
Connor Bren <wildweegee101@gmail.com>
2024-12-02 02:45:59 -0700

    day 1, first revision

--- 1p2.lua
+++ 1p2.lua
@@ -4,25 +4,17 @@ local file = io.open("1.txt", "r")
 if file == nil then error("Input file was not found") end
 
 local list_1 = {}
-local list_2 = {}
 
 local line = file:read("l")
+local occurances = {}
 while line ~= nil do
     local first, second = line:match("(%d+)   (%d+)")
-    list_1[#list_1+1], list_2[#list_2+1] = tonumber(first), tonumber(second)
+    list_1[#list_1+1], second = tonumber(first), tonumber(second)
+    if second == nil then error("Parsing failed") end
+
+    occurances[second] = (occurances[second] or 0) + 1
+
     line = file:read("l")
-end
-
-table.sort(list_1)
-table.sort(list_2)
-
-local occurances = {}
-for _, v in ipairs(list_2) do
-    if occurances[v] == nil then
-        occurances[v] = 1
-    else
-        occurances[v] = occurances[v] + 1
-    end
 end
 
 local similarity = 0
